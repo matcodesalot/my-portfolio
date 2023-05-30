ebc8d5a25f669251da5b4c83e1f36c93a3dd758f
matcodesalot <reallymathew@gmail.com>
2023-05-30 17:58:51 -0500

    change nav buttons to a href

--- main.js
+++ main.js
@@ -1,27 +1,18 @@
 const text = "I promise this will get better.";
 const typingDelay = 50; // Delay in milliseconds between each character
-const typingTextEl = document.getElementById("typing-text");
+const typingText = document.getElementById("typing-text");
 const menuButton = document.querySelector('.menu-button');
 
 let i = 0;
 function typeNextCharacter() {
     if (i < text.length) {
-        typingTextEl.innerHTML += text.charAt(i);
+        typingText.innerHTML += text.charAt(i);
         i++;
         setTimeout(typeNextCharacter, typingDelay);
     }
 }
-
-function openLinkedIn() {
-    window.open("https://www.linkedin.com/in/mathew-johns/", "_blank");
-}
-
-function openGitHub() {
-    window.open("https://github.com/matcodesalot", "_blank");
-}
+typeNextCharacter();
 
 function toggleMenu() {
     menuButton.classList.toggle('open');
 }
-
-typeNextCharacter();
