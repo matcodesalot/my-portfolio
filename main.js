c851d8980af103b1a2be4b51e2a095473c0ffe24
matcodesalot <reallymathew@gmail.com>
2023-06-07 21:35:51 -0500

    little refinement

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 const menuButton = document.querySelector(".hamburger");
 const navigation = document.getElementById("navigation");
+const socialLinks = document.querySelectorAll(".sci li a");
 
 function toggleMenu() {
     menuButton.classList.toggle("open");
     if(navigation.style.display === "grid") {
         navigation.style.display = "none";
+        socialLinks[0].style.display = "grid";
+        socialLinks[1].style.display = "grid";
     }
     else {
         navigation.style.display = "grid";
+        socialLinks[0].style.display = "none";
+        socialLinks[1].style.display = "none";
     }
 }
