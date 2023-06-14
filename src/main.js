b35e662043bceec93cfe7def1d9ad386d32718c5
matcodesalot <reallymathew@gmail.com>
2023-06-14 17:02:18 -0500

    mobile menu test

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 const menuButton = document.querySelector(".hamburger");
+const navContainer = document.querySelector(".container");
 
 function toggleMenu() {
     menuButton.classList.toggle("open");
+    if(navContainer.style.display === "flex") {
+        navContainer.style.display = "none";
+    }
+    else {
+        navContainer.style.display = "flex";
+    }
 }
